f2659dabd844912c51a03b4a2a39c41ea3b61e4a
Balint Haui <balint.haui@gmail.com>
2021-06-28 14:40:53 +0200

    basic routing for pageComponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import classes from './App.module.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
+import Login from './pageComponents/Login';
+import UserDash from './pageComponents/UserDash';
+import AdminDash from './pageComponents/AdminDash';
 
 function App() {
 
   return (
-    <div className={`wrapper ${classes.appWrapper}`}>
-      <h2 className={classes.h2}>Hello</h2>
+    <div>
+      <Switch>
+        <Route path="/login" >
+          <Login></Login>
+        </Route>
+        <Route path="/dashboard" >
+          <UserDash></UserDash>
+        </Route>
+        <Route path="/admin" >
+          <AdminDash></AdminDash>
+        </Route>
+      </Switch>
     </div>
   );
+
 }
 
 export default App;
