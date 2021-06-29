2a940dfa474db3ecfeb96c874b8bdf39483328e9
Balint Haui <balint.haui@gmail.com>
2021-06-29 17:26:53 +0200

    user form

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const apiBase = 'https://wsp-api1979.herokuapp.com';
+
+export const localApi = 'http://localhost:7171';
--- src/reusableComponents/LoginRegisterForm/LoginRegisterForm.js
+++ src/reusableComponents/LoginRegisterForm/LoginRegisterForm.js
@@ -1,6 +1,7 @@
 import classes from './LoginRegisterForm.module.css';
 import useInput from './../../customHooks/use-input';
 import { emailValidatorFn, passwordValidatorFn } from './../../utils/validatorFunctions';
+import { apiBase, localApi } from './../../constants/constants';
 
 const LoginRegisterForm = () => {
 
@@ -22,8 +23,25 @@ const LoginRegisterForm = () => {
 
     const formIsValid = !emailHasError && !passwordHasError;
 
-    const submitHandler = e => {
+    const submitHandler = async (e) => {
         e.preventDefault();
+
+        const userObj = {
+            action: 'register',
+            email,
+            password,
+        };
+
+        fetch(`${localApi}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userObj)
+        })
+            .then(async (response) => await response.json())
+            .then(data => console.log(data))
+            .catch(err => alert(err.message));
 
         resetEmail();
         resetPassword();
