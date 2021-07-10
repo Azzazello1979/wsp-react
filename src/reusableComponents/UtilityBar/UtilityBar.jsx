7b3b3fe0d8e618f27ad2855c9236d41ea81a19ac
Balint Haui <balint.haui@gmail.com>
2021-07-10 16:14:57 +0200

    utility bar update

--- src/reusableComponents/UtilityBar/UtilityBar.jsx
+++ src/reusableComponents/UtilityBar/UtilityBar.jsx
@@ -1,9 +1,13 @@
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
+
+import HistoryIcon from '@material-ui/icons/History';
+import FaceIcon from '@material-ui/icons/Face';
+import HomeIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FilterListIcon from '@material-ui/icons/FilterList';
+
 import { useState } from 'react';
 import classes from './UtilityBar.module.css';
 
@@ -16,11 +20,13 @@ const UtilityBar = () => {
     };
 
     return (
-        <BottomNavigation value={value} onChange={handleChange} classes={{ root: classes['bottom-navigation-root'] }} >
-            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Recents" value="recents" icon={<RestoreIcon />} />
+        <BottomNavigation value={value} onChange={handleChange}>
+            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Home" value="home" icon={<HomeIcon />} />
+            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Filter" value="filter" icon={<FilterListIcon />} />
             <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Folder" value="folder" icon={<FolderIcon />} />
+            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Orders" value="history" icon={<HistoryIcon />} />
+            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="User" value="user" icon={<FaceIcon />} />
+            <BottomNavigationAction classes={{ root: classes['bottom-navigation-action-root'], selected: classes.selected }} label="Cart" value="cart" icon={<ShoppingCartIcon />} />
         </BottomNavigation>
     );
 };
